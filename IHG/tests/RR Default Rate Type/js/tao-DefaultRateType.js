45f5ccc6a3c6302f10ba10b688fcc4e649c63d9f
Chris d'Aquin <chris.daquin@ihg.com>
2015-08-04 15:24:49 -0400

    RR Default Rate Type
    initial check-in for RR Default Rate Type.  Got redirect working that
    insures Rate Type is the selected sort type for RR pages.

---
+++ IHG/tests/RR Default Rate Type/js/tao-DefaultRateType.js
@@ -0,0 +1,19 @@
+﻿// Grab the current URL and put it into a string for evaluation
+var taoLocation = window.location.toString();
+debugger;
+
+// See if the sortby=Rate Type is already in the URL.  If not, then hide the
+// page, change the sortby value to Rate Type, and replace this page with the
+// right one.
+if (taoLocation.indexOf("qRRSrt=rc") < 0) {
+
+    Bootstrapper.MVT.injectCSS("body { display:none; }");
+
+    var taoNewLocation = taoLocation.replace("qRRSrt=rt", "qRRSrt=rc");
+    window.location.replace(taoNewLocation);
+
+}
+
+
+// window.location = trackingJson.urlType + '://' + document.location.host + document.location.pathname + '/map' + document.location.search;
+
