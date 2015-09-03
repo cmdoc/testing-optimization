87e019955b870554283ab73002b3da0e715d03c9
Chris d'Aquin <chris.daquin@ihg.com>
2015-09-03 10:42:00 -0400

    initial check-in
    This is a simple design.  All code works and is final.

---
+++ IHG/tests/RR Points and Cash Sort Order/js/tao-PCsortOrder_C.js
@@ -0,0 +1,22 @@
+﻿jQuery(document).ready(function () {
+
+    // Grab all of the .rewardRate <div>s, loop through them, and 
+    // change the order of the radio buttons
+    jQuery(".rewardRate").each(function () {
+        var taoRadio1 = jQuery(this).children("div").eq(0);
+        var taoRadio2 = jQuery(this).children("div").eq(1);
+        var taoRadio3 = jQuery(this).children("div").eq(2);
+        var taoNewOrder = "";
+
+        // If the third item doesn't exist, then don't include it.
+        if (taoRadio3.html() != undefined) {
+            taoNewOrder = "<div>" + taoRadio3.html() + "</div><div>" + taoRadio2.html() + "</div><div>" + taoRadio1.html() + "</div>";
+        } else {
+            taoNewOrder = "<div>" + taoRadio2.html() + "</div><div>" + taoRadio1.html() + "</div>";
+        }
+
+        jQuery(this).html(taoNewOrder);
+    });
+
+
+});
