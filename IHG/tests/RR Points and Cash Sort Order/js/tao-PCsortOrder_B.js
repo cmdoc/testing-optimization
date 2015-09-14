0fe61bdb83f5987b7bbb4e9ab2ecdf766914891b
Chris d'Aquin <chris.daquin@ihg.com>
2015-09-14 12:12:05 -0400

    unavailable fix
    Fixed the bug that popped up where no more rooms were available for
    Points and Cash.

--- IHG/tests/RR Points and Cash Sort Order/js/tao-PCsortOrder_B.js
+++ IHG/tests/RR Points and Cash Sort Order/js/tao-PCsortOrder_B.js
@@ -3,21 +3,24 @@
     // Grab all of the .rewardRate <div>s, loop through them, and 
     // change the order of the radio buttons
     jQuery(".rewardRate").each(function () {
-        debugger;
+        var taoRadioCount = jQuery(this).children("div").length;
         var taoRadio1 = jQuery(this).children("div").eq(0);
         var taoRadio2 = jQuery(this).children("div").eq(1);
         var taoRadio3 = jQuery(this).children("div").eq(2);
         var taoNewOrder = "";
 
+        // If there are any radio button options, then re-arrange their order.
         // If the third item doesn't exist, then don't include it.
-        if (taoRadio3.html() != undefined) {
-            taoNewOrder = "<div>" + taoRadio1.html() + "</div><div>" + taoRadio3.html() + "</div><div>" + taoRadio2.html() + "</div>";
-        } else {
-            taoNewOrder = "<div>" + taoRadio1.html() + "</div><div>" + taoRadio2.html() + "</div>";
+        if (taoRadioCount > 0) {
+            if (taoRadio3.html() != undefined) {
+                taoNewOrder = "<div>" + taoRadio1.html() + "</div><div>" + taoRadio3.html() + "</div><div>" + taoRadio2.html() + "</div>";
+            } else {
+                taoNewOrder = "<div>" + taoRadio1.html() + "</div><div>" + taoRadio2.html() + "</div>";
+            }
+
+            jQuery(this).html(taoNewOrder);
         }
 
-        jQuery(this).html(taoNewOrder);
     });
-
 
 });
