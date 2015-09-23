2260173d274b2f1c7735f2488edf73b7e056203c
Chris d'Aquin <chris.daquin@ihg.com>
2015-09-23 08:12:54 -0400

    Initial Check-in
    Checking in all files created for RR Room Urgency Messaging campaign.
    All of this looks like it meets test plan requirements.  Will be doing
    Target verification in a few days (must wait for a test to clear out of
    Target priority first).

---
+++ EX/tests/rrUrgencyMessages/js/taoUrgencyMsgC.js
@@ -0,0 +1,16 @@
+﻿// Hide the current urgency message before doing any of the changes.
+Bootstrapper.MVT.injectCSS("span.roomTypeDesc > div.urgentMsgCss { display:none; }");
+
+jQuery(document).ready(function () {
+
+    // Go through each of the targeted urgency messages and change their 
+    // CSS classes to display them the way we want.
+    jQuery("span.roomTypeDesc > div.urgentMsgCss").each(function () {
+        jQuery(this).find("img").attr("class", "urgentMsgImg taoUrgencyClockGreen");
+        jQuery(this).find("span").eq(0).attr("class", "taoUrgencyMsg taoGreen");
+        jQuery(this).find("span").eq(1).addClass("taoUrgencyTextGreen");
+    });
+
+    // Now the changes are done, so show the new look
+    jQuery("span.roomTypeDesc > div.urgentMsgCss").show();
+});
---
+++ EX/tests/rrUrgencyMessages/js/taoUrgencyMsgD.js
@@ -0,0 +1,2 @@
+﻿// Hide the current urgency message 
+Bootstrapper.MVT.injectCSS("span.roomTypeDesc > div.urgentMsgCss { display:none; }");
