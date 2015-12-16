3e5cc9e2e58a4e22af43893d170ab5dafb0b783d
Chris d'Aquin <chris.daquin@ihg.com>
2015-12-16 09:26:41 -0500

    First Phase done
    Put together jQuery that will move filter dropdowns into DIVs that can
    be manipulated.

---
+++ EX/tests/refineSearch/js/expB.js
@@ -0,0 +1,45 @@
+// Insert a DIV for the Distance from Destination block
+jQuery("<div id='taoDistance'></div>").insertAfter("#filterSortForm fieldset div.col-md-12");
+
+// Grab the distance dropdown, radio buttons, and title
+var $taoDistanceSelect = jQuery("#distanceFilter");
+var $taoDistanceDiv = jQuery("#distanceFilter").next();
+var $taoDistanceMiles = jQuery("#distanceFilter").siblings("label").eq(0);
+var $taoDistanceKilos = jQuery("#distanceFilter").siblings("label").eq(1);
+var $taoDistanceTitle = jQuery("#distanceFilter").siblings("span");
+
+// Move everything to the new DIV
+jQuery("#taoDistance").append($taoDistanceTitle);
+jQuery("#taoDistance span").text("Distance from Destination");
+jQuery("#taoDistance").append($taoDistanceSelect);
+jQuery("#taoDistance").append($taoDistanceDiv);
+jQuery("#taoDistance").append($taoDistanceMiles);
+jQuery("#taoDistance").append($taoDistanceKilos);
+
+// Insert the DIVs that I will need to move around the dropdowns in the filter
+jQuery("<div id='taoFilterBy'></div>").insertAfter("#filterSortForm fieldset div.col-md-12");
+jQuery("<div id='taoAmenities'></div>").insertAfter("#filterSortForm fieldset div.col-md-12");
+jQuery("<div id='taoIHGBrands'></div>").insertAfter("#filterSortForm fieldset div.col-md-12");
+jQuery("<div id='taoGuestRatings'></div>").insertAfter("#filterSortForm fieldset div.col-md-12");
+
+// Grab the SELECT and button-grp DIV for each dropdown
+var $taoGuestsRatingsSelect = jQuery("#ratingsFilter");
+var $taoGuestRatingsDiv = jQuery("#ratingsFilter").next();
+var $taoAmenitiesSelect = jQuery("#amenitiesFilter");
+var $taoAmenitiesDiv = jQuery("#amenitiesFilter").next();
+var $taoIHGBrandsSelect = jQuery("#brandsFilter");
+var $taoIHGBrandsDiv = jQuery("#brandsFilter").next();
+var $taoFilterBySelect = jQuery("#miscFilter");
+var $taoFilterByDiv = jQuery("#miscFilter").next();
+
+// Move the four pairs into the new DIVs the were created
+jQuery("#taoGuestRatings").append($taoGuestsRatingsSelect);
+jQuery("#taoGuestRatings").append($taoGuestRatingsDiv);
+jQuery("#taoAmenities").append($taoAmenitiesSelect);
+jQuery("#taoAmenities").append($taoAmenitiesDiv);
+jQuery("#taoIHGBrands").append($taoIHGBrandsSelect);
+jQuery("#taoIHGBrands").append($taoIHGBrandsDiv);
+jQuery("#taoFilterBy").append($taoFilterBySelect);
+jQuery("#taoFilterBy").append($taoFilterByDiv);
+
+// Organize the new DIVs so they stack correctly
