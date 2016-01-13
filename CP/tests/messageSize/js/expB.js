4c16f42a91e3e1c88cae3ae1538d039cb7035ac5
cmdoc <cmdoc@yahoo.com>
2016-01-13 07:40:49 -0500

    Final check in
    The code is finished for the message size campaign

---
+++ CP/tests/messageSize/js/expB.js
@@ -0,0 +1,12 @@
+﻿// Hide the old form before doing any of the changes.
+Bootstrapper.MVT.injectCSS("#reserveNowMsg { display:none; }");
+
+jQuery(document).ready(function () {
+
+    // Change the dimensions of the checkbox icon
+    jQuery(".reserveNowCheckBox").attr("width", "14").attr("height", "14");
+
+    // Show the message
+    jQuery("#reserveNowMsg").show();
+
+});
---
+++ CP/tests/messageSize/js/expC.js
@@ -0,0 +1,12 @@
+﻿// Hide the old form before doing any of the changes.
+Bootstrapper.MVT.injectCSS("#reserveNowMsg { display:none; }");
+
+jQuery(document).ready(function () {
+
+    // Change the dimensions of the checkbox icon
+    jQuery(".reserveNowCheckBox").attr("width", "11").attr("height", "11");
+
+    // Show the message
+    jQuery("#reserveNowMsg").show();
+
+});
