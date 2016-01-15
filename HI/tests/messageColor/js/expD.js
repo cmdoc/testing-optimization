73c10c23cc44494cd32e34a60d5b5f8f408a161f
cmdoc <cmdoc@yahoo.com>
2016-01-15 12:08:15 -0500

    Initial Check in
    This is the initial check in of files.  Based off of what David approved
    for the Crowne Plaza version, this aught to be approved.

---
+++ HI/tests/messageColor/js/expD.js
@@ -0,0 +1,14 @@
+﻿// Hide the old form before doing any of the changes.
+Bootstrapper.MVT.injectCSS("#reserveNowMsg { display:none; }");
+
+jQuery(document).ready(function () {
+
+    // Change out the source for the checkbox icon and add the CSS class 
+    // that will display it correctly
+    jQuery("img.reserveNowCheckBox").attr("src", "http://prodcache.internal.ihg.com/content/dam/etc/media_library/cn/0/cn/misc/sprite/taoUrgChecks.png");
+    jQuery("img.reserveNowCheckBox").addClass("taoCheckBoxSpriteWhite");
+
+    // Show the row now that everything is done
+    jQuery("#reserveNowMsg").show();
+
+});
