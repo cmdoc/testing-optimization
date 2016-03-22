c86c93fd3da37302e20a73a101ed562041ca10fa
cmdoc <cmdoc@yahoo.com>
2016-03-22 11:49:22 -0400

    final check-in for Hover BPG

---
+++ SB/tests/Hover BPG/js/expB.js
@@ -0,0 +1,14 @@
+﻿jQuery("document").ready(function () {
+    
+    // Create a div and insert it under the Check Rates button
+    jQuery("<div class='taoHoverBPG'><img src='../images/taoBrownCheck15x15.png' />Best Price Guarantee</div>").insertAfter(jQuery(".bookAnchor"));
+
+    jQuery(".checkRates .bookAnchor").on("mouseover", function () {
+        jQuery(this).closest(".row").find(".taoHoverBPG").show();
+    });
+
+    jQuery(".checkRates .bookAnchor").on("mouseout", function () {
+        jQuery(this).closest(".row").find(".taoHoverBPG").hide();
+    });
+
+});
