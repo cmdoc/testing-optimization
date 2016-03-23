7984b0e560aaa7dfab5d5bb8d91413186bc6a4f0
cmdoc <cmdoc@yahoo.com>
2016-03-23 15:18:37 -0400

    Exp B check-in for Book Direct Messaging
    Finished the code for Experiece B

---
+++ EX/tests/Book Direct Messaging/js/expB.js
@@ -0,0 +1,16 @@
+﻿/*
+<div id="taoIconList">
+    <p class="taoBkfst"><img src="">Complimentary Breakfast</p>
+    <p class="taoWiFi"><img src="">Free High-Speed Internet</p>
+    <p class="taoPrice"><img src="">Best Price Guaranteed</p>
+</div>
+*/
+
+Bootstrapper.MVT.injectCSS("#LIRGGuestInfoLeftNav { display:none; }");
+
+jQuery(document).ready(function () {
+
+    jQuery("#LIRGGuestInfoLeftNav").replaceWith(jQuery("#taoIconList"));
+    jQuery("#taoIconList").show();
+
+});
