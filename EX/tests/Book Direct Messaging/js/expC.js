ba6fe1b8b88f50e0f357dd875363e4a3a8920fdf
cmdoc <cmdoc@yahoo.com>
2016-03-23 11:28:46 -0400

    Exp C check-in
    Got everything working for Experience C. Checking in icon PNG file.

---
+++ EX/tests/Book Direct Messaging/js/expC.js
@@ -0,0 +1,14 @@
+﻿/*
+<div id="taoIconList">
+    <p class="taoBkfst"><img src="">Complimentary Breakfast</p>
+    <p class="taoWiFi"><img src="">Free High-Speed Internet</p>
+    <p class="taoPrice"><img src="">Best Price Guaranteed</p>
+</div>
+*/
+
+jQuery(document).ready(function () {
+
+    jQuery("#taoIconList").insertAfter(jQuery(".sideColumn_callout").eq(0).find("address"));
+    jQuery("#taoIconList").show();
+
+});
