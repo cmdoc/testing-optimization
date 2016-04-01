7ff356296c03367a89d6a6118b0636f7065ca34a
cmdoc <cmdoc@yahoo.com>
2016-04-01 10:36:55 -0400

    Final updates
    These are the final updates to the code for the Are You Sure campaign.

--- CP/tests/Are You Sure/js/expBD.js
+++ CP/tests/Are You Sure/js/expBD.js
@@ -1,7 +1,4 @@
-﻿// Hide the old form before doing any of the changes.
-// Bootstrapper.MVT.injectCSS("#reserveNowMsg { display:none; }");
-
-jQuery(document).ready(function () {
+﻿jQuery(document).ready(function () {
 
     // Build the DIV that contains the "Are you sure?" messaging and place
     // it in the right position so it can be shown when needed.
@@ -11,10 +8,6 @@ jQuery(document).ready(function () {
 
     // When the "No" option is clicked for Quick Enroll, show or hide the 
     // "Are you sure?" messaging.
-    //jQuery("#pcrNoEnroll").on("change", function () { 
-    //    jQuery("#taoAreYouSure").toggle();
-    //});
-
     jQuery("input[name='pcrQuickEnroll']").click(function () {
         if (jQuery(this).val() == "false") {
             jQuery("#taoAreYouSure").show();
