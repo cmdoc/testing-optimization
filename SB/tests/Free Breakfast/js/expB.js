05c77572e0a8bfcb85f111c227b869002470be30
cmdoc <cmdoc@yahoo.com>
2016-05-09 11:08:33 -0400

    Free Breakfast - initial check-in

---
+++ SB/tests/Free Breakfast/js/expB.js
@@ -0,0 +1,6 @@
+﻿jQuery(document).ready(function () {
+
+    // Look up all rows for rates and insert #taoFreeBrkfst DIV 
+    jQuery("<div class='taoFreeBrkfst'>This room includes free breakfast and Internet!</div>").insertAfter(".headerWrapper span.roomLongDescription");
+    
+});
