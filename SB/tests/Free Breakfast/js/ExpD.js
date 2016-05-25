4427d0354ba459a16174b199a18cc4de819c62e9
cmdoc <cmdoc@yahoo.com>
2016-05-25 10:38:34 -0400

    Free Breakfast - final check in
    Checking in Exp D

---
+++ SB/tests/Free Breakfast/js/ExpD.js
@@ -0,0 +1,14 @@
+﻿jQuery(document).ready(function () {
+
+    // remove the ugly checkmark
+    jQuery(".reserveNowMsgImgDiv").remove();
+
+    // create a var to store the new html
+    var taoCheck = '<svg class="taoCheckStyles"><use xlink:href="#taoCheckMark" /></svg>';
+    var taoNewMsg = jQuery("<span>ALL ROOMS INCLUDE" + taoCheck + "Complimentary Breakfast" + taoCheck + "Free High-Speed Internet" + taoCheck + "Best Price Guarantee</span>");
+
+    // replace the text with the new copy
+    jQuery(".reserveNowMsgTextDiv").html(taoNewMsg);
+
+});
+
