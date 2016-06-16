05989aed52a8867044b476fdbadfb3b22dbd51e3
cmdoc <cmdoc@yahoo.com>
2016-06-16 11:21:47 -0400

    YR Block - initial check-in
    Initial check-in of YOUR RATE Block code.  Experiences B and C are
    already complete.

---
+++ HI/tests/YOUR RATE Block Optimization/js/expC.js
@@ -0,0 +1,16 @@
+﻿jQuery(document).ready(function () {
+
+    // Find all of the crossed out (old) prices in the YOUR RATE blocks
+    // and remove them.
+    jQuery("div#memberExclusivePriceInfoArea span.priceBefore").remove();
+
+    // Find all of the existing "Member Discount" DIVs and remove them
+    jQuery("div#memberExclusivePriceInfoArea div.breakfastLabel").remove();
+
+    // Insert new "MEMBER DISCOUNT" span
+    jQuery("div.memberRateTypeLineItemRight").prepend("<span class='taoMemberDiscount'>member discount</span>");
+
+    // Insert new text div for wording in grey box
+    jQuery("div.memberRateTypeLineItemRight .rateSelectionArea").append("<div class='taoGreyBoxText'><span class='taoGreyBoxTextBold'>Not a member yet?</span><br />Choose this great rate and<br /><span class='taoGreyBoxTextMango'>join for free</span> while you book.</div>");
+
+});
