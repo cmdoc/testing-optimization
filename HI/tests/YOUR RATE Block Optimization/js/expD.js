016146d6be1d4b88de514ed4ddfc79920f192cfe
cmdoc <cmdoc@yahoo.com>
2016-06-17 08:34:15 -0400

    YR Block - Exp D
    Check in for all of the Experience D code.

---
+++ HI/tests/YOUR RATE Block Optimization/js/expD.js
@@ -0,0 +1,16 @@
+﻿jQuery(document).ready(function () {
+
+    // Find all of the crossed out (old) prices in the YOUR RATE blocks
+    // and remove them.
+    jQuery("div#memberExclusivePriceInfoArea span.priceBefore").remove();
+
+    // Find all of the existing "Member Discount" DIVs and remove them
+    jQuery("div#memberExclusivePriceInfoArea div.breakfastLabel").remove();
+
+    // Insert new "MEMBER DISCOUNT" span
+    jQuery("div.memberRateTypeLineItemRight").prepend("<span class='taoMemberDiscount'>member discount</span>");
+
+    // Insert new text div for wording in grey box
+    jQuery("div.memberRateTypeLineItemRight .rateSelectionArea").append("<div class='taoGreyBoxText'><div class='taoArrow'></div><span class='taoGreyBoxTextBold'>Not a member?</span> Join for <br /><span class='taoGreyBoxTextMango'>free</span> while you book!</div>");
+
+});
