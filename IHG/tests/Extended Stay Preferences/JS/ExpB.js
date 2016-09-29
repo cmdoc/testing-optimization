8e8b18a1fc84b4057479eb2b57ffe542e47fd517
cmdoc <cmdoc@yahoo.com>
2016-09-29 09:05:42 -0400

    Extended Stay - final changes
    Final changes for this project. This has cleared QA.

--- IHG/tests/Extended Stay Preferences/JS/ExpB.js
+++ IHG/tests/Extended Stay Preferences/JS/ExpB.js
@@ -49,45 +49,101 @@ function taoReSortResults() {
 
 }
 
+function taoDetermineNumOfDays() {
+    // Figure out the dates being searched for. These are grabbed from
+    // the URL and need to be formatted to a Date object so we
+    // can determine how many total consecutive days are being booked.
+    // This code will work for all dates, whether they span over two
+    // months or over two different years. It should also correctly
+    // accomodate leap years.
+    var taoCheckInDay = getUrlParameter("qCiD"); // 01 through 31
+    var taoCheckOutDay = getUrlParameter("qCoD"); // 01 through 31
+    var taoCheckInMonth = getUrlParameter("qCiMy"); // 002016 (Jan) through 112016 (Dec)
+    var taoCheckOutMonth = getUrlParameter("qCoMy"); // 002016 (Jan) through 112016 (Dec)
+    var taoCheckInYear = taoCheckInMonth.substring(2, 6); // Should just get 2016
+    var taoCheckOutYear = taoCheckOutMonth.substring(2, 6); // Should just get 2016
+    taoCheckInMonth = taoCheckInMonth.substring(0, 2); // Should now just be 00 through 11
+    taoCheckOutMonth = taoCheckOutMonth.substring(0, 2); // Should now just be 00 through 11
+
+    // Now that we have all of the parts, let's piece them together
+    // into a real Date object.
+    var taoCheckInDate = new Date(taoCheckInYear, taoCheckInMonth, taoCheckInDay);
+    var taoCheckOutDate = new Date(taoCheckOutYear, taoCheckOutMonth, taoCheckOutDay);
+
+    // Now take those dates and figure out how many consecutive days the
+    // search is for
+    return daydiff(taoCheckInDate, taoCheckOutDate);
+}
+
+function daydiff(first, second) {
+    // Takes two Date objects and determines how many days are between them.
+    return Math.round((second - first) / (1000 * 60 * 60 * 24));
+}
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    // make a function to pull out values from the GET url. Call it like this:
+    //      var blog = getUrlParameter('blog');
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
 ///////////////////////////////////////////////////////////////////////////////
 // M A I N 
 jQuery(document).ready(function () {
 
-    // Re-order the search results and feature the extended stay hotels at the
-    // top of the list
-    taoReSortResults();
+    // Get the number of days for this search just once and then use it
+    // multiple times.
+    var taoNumOfDays = taoDetermineNumOfDays();
+
+    // If taoTotalDays is 5 or greater, then do a couple of things....
+    if (taoNumOfDays >= 5) {
+
+        // Re-order the search results and feature the extended stay hotels at
+        // the top.
+        taoReSortResults();
+
+        // Track for when the user clicks on the "Show More" or "Show All"
+        // buttons. If the number of days is 5 or greater, then when the
+        // re-sort the page when ajaxComplete() finishes.
+        jQuery("body").on("click", "#showMoreLink, #showAllLink", function () {
+
+            localStorage.setItem("taoShowClick", "true");
+
+            // Wait until the ajax call is complete then re-sort the results
+            jQuery(document).ajaxComplete(function (event, request, settings) {
+
+                if (localStorage.getItem("taoShowClick") == "true") {
+
+                    // Send the command to resort the results
+                    taoReSortResults();
+
+                    // Figure out how many rows there are and set the scroll
+                    // time to 50 milliseconds per
+                    var taoResRows = jQuery(".resRow").length * 50;
+
+                    // Show the results area and scroll to the top
+                    $('html, body').animate({scrollTop: 285}, taoResRows);
+
+                }
+
+                localStorage.setItem("taoShowClick", "false");
+
+            });
+
+        });
+    }
 
     // Show the search results
     jQuery("div#searchResultContainer").show();
-
-    // Track for when the user clicks on the "Show More" or "Show All"
-    // buttons. If the number of days is 5 or greater, then when the 
-    // re-sort the page when ajaxComplete() finishes.
-    jQuery("body").on("click", "#showMoreLink, #showAllLink", function () {
-
-        localStorage.setItem("taoShowClick", "true");
-
-        // Wait until the ajax call is complete then re-sort the results
-        jQuery(document).ajaxComplete(function (event, request, settings) {
-
-            if (localStorage.getItem("taoShowClick") == "true") {
-
-                // Send the command to resort the results
-                taoReSortResults();
-
-                // Figure out how many rows there are and set the scroll
-                // time to 50 milliseconds per
-                var taoResRows = jQuery(".resRow").length * 50;
-
-                // Show the results area and scroll to the top
-                $('html, body').animate({scrollTop: 285}, taoResRows);
-
-            }
-
-            localStorage.setItem("taoShowClick", "false");
-
-        });
-
-    });
 
 });
