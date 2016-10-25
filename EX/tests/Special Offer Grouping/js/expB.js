03e2d55fa18a7bbbfafe1493e7abba31ddddb3a6
cmdoc <cmdoc@yahoo.com>
2016-10-25 09:01:40 -0400

    Special Offer Group - CSS update
    Code works and moves the right rates to the right place.  Now adding
    HTML and CSS styling to match the mockups. This check in has the top and
    bottom buttons and they properly open and close the hidden area. Bottom
    button needs to be moved to right spot, border color needs to be fixed
    for hidden area, and SVG icons need to be added to buttons.

--- EX/tests/Special Offer Grouping/js/expB.js
+++ EX/tests/Special Offer Grouping/js/expB.js
@@ -1,29 +1,53 @@
 jQuery("document").ready(function(){
 
-    jQuery(".regularRates, .secondaryRates").each(function(i) {
+    // Start working on the page, hitting one rate type at a time
+    jQuery(".rateTypeLineItems").each(function(i) {
+
+        // Put this .rateTypeLineItems instance into a variable for future
+        // referencing.
+        var $taoThisRoom = jQuery(this);
 
         // create a div for any rates we find that we need to move
-        var $taoSpecialOfferGroupingDiv = jQuery("<div class='taoSpecialOfferGroup' id='taoSOG" + i + "'></div>");
+        var $taoSpecialOfferGroupingDiv = jQuery("<div class='taoSpecialOfferGroup'></div>");
+        $taoSpecialOfferGroupingDiv.append("<div class='taoSOGHeader taoSOGHeaderPadding'><input class='cssButton bcex' value='Special Offers &amp; Deals Go Here'>");
+        $taoSpecialOfferGroupingDiv.append("<div class='taoSOGrates' id='taoSOG" + i + "'></div>");
+        $taoThisRoom.prepend($taoSpecialOfferGroupingDiv);
 
-        // Look for any that does not contain an div.upSellContainter or
-        // div.spotlightPointsAndCash. We have some work to do with those.
-        if (jQuery(this).find("div.upSellContainer").length == 0 || jQuery(this).find("div.spotlightPointsAndCash").length == 0) {
+        // Go through all of the rates for this room and move the special
+        // offers and secondary rates to the new div
+        $taoThisRoom.find(".regularRates, .secondaryRates").each(function() {
 
-            // Put this into the new div we just created
+            // Look for any that does not contain an div.upSellContainter or
+            // div.spotlightPointsAndCash. We have some work to do with those.
+            if (jQuery(this).find("div.upSellContainer").length == 0 && jQuery(this).find("div.spotlightPointsAndCash").length == 0) {
 
-        }
+                // Put this into the new div we just created
+                jQuery("#taoSOG" + i).append(jQuery(this));
+//                $taoSpecialOfferGroupingDiv.append(jQuery(this));
+            }
+
+        });
+
+        // Add a Hide button to the new div
+        jQuery("#taoSOG" + i).append("<div class='taoSOGFooter'><input class='cssButton bcex' value='Hide Special Offers &amp; Deals'>");
 
     });
 
-    // Now that we are done looping through all of the rate offerings, take
-    // the rates we collected and put them in their new location
-
-
-    // Create the new HTML/CSS for the Special Offers & Deals button
-
-
     // Put in all the code and functionality to cause the new button to open
     // and close the grouped rates.
+    jQuery(".taoSOGHeader, .taoSOGFooter").on("click", "input", function () {
 
+        // find the parent div
+        var $taoSOGDiv = jQuery(this).closest(".taoSpecialOfferGroup");
+
+        // toggle the .taoSOGpadding and .taoSOGHeaderPadding classes so the
+        // open or closed group has the right padding
+        $taoSOGDiv.toggleClass("taoSOGpadding");
+        $taoSOGDiv.find(".taoSOGHeader").toggleClass("taoSOGHeaderPadding");
+
+        // toggle the rates being offered in this .taoSpecialOfferGroup
+        $taoSOGDiv.find(".taoSOGrates").toggle(400);
+
+    });
 
 });
