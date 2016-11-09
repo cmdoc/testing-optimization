68dd55ef0252c7a6e9981a381dbd1ec2bac67fc0
cmdoc <cmdoc@yahoo.com>
2016-11-09 13:17:55 -0500

    Special Offer Group - IHG bug fix
    Fixing the same bug in the IHG version

--- EX/tests/Special Offer Grouping/js/expB-IHG.js
+++ EX/tests/Special Offer Grouping/js/expB-IHG.js
@@ -2,8 +2,10 @@ jQuery("document").ready(function(){
 
     // Create image tags for the plus and minus icons for use in the SOGHeader
     // and SOGFooter
-    var taoCirclePlus = "<svg class='taoCirclePlus'><use xlink:href='#tao_circle_plus' /></svg>";
-    var taoCircleMinus = "<svg class='taoCircleMinus'><use xlink:href='#tao_circle_minus' /></svg>";
+    var taoCirclePlusHref = "https://ihg-my.sharepoint.com/personal/chris_daquin_ihg_com/_layouts/15/guestaccess.aspx?guestaccesstoken=NI3JGUXvFU1Edem4jW40rbx1j3hDG1ORgA2eHlNrMvo%3d&docid=071c63e9874ee4975a2f421c2a80c2070&rev=1";
+    var taoCircleMinusHref = "https://ihg-my.sharepoint.com/personal/chris_daquin_ihg_com/_layouts/15/guestaccess.aspx?guestaccesstoken=556cMcaijVcAzM%2bUgvQ0ubkDwkiltBHb53ZiyrhTZgU%3d&docid=03941d06e633344d49850e91604c85ecb&rev=1";
+    var taoCirclePlus = "<img class='taoCirclePlus' href='"+ taoCirclePlusHref + "' />";
+    var taoCircleMinus = "<img class='taoCircleMinus' href='"+ taoCircleMinusHref + "' />";
 
     // Start working on the page, hitting one rate type at a time
     jQuery(".rateTypeLineItems").each(function(i) {
@@ -40,20 +42,20 @@ jQuery("document").ready(function(){
 
     // Put in all the code and functionality to cause the new button to open
     // and close the grouped rates.
-    jQuery(".taoSOGHeader span, .taoSOGFooter input, svg.taoCirclePlus, svg.taoCircleMinus").on("click", function () {
+    jQuery(".taoSOGHeader span, .taoSOGFooter input, img.taoCirclePlus, img.taoCircleMinus").on("click", function () {
 
         // find the parent div
         var $taoSOGDiv = jQuery(this).closest(".taoSpecialOfferGroup");
 
         // identify the svg tags in the header and footer.
-        var $taoSOGHeaderSVG = $taoSOGDiv.find(".taoSOGHeader svg use");
+        var $taoSOGHeaderImg = $taoSOGDiv.find(".taoSOGHeader img");
 
-        // Figure out what SVG icon is being referred to and change it to
+        // Figure out what icon is being referred to and change it to
         // reference the other one
-        if ($taoSOGHeaderSVG.attr("xlink:href") == "#tao_circle_plus") {
-            $taoSOGHeaderSVG.attr("xlink:href", "#tao_circle_minus");
+        if ($taoSOGHeaderImg.hasClass("taoCirclePlus")) {
+            $taoSOGHeaderImg.attr("href", taoCircleMinusHref).removeClass("taoCirclePlus").addClass("taoCircleMinus");
         } else {
-            $taoSOGHeaderSVG.attr("xlink:href", "#tao_circle_plus");
+            $taoSOGHeaderImg.attr("href", taoCircleMinusHref).removeClass("taoCircleMinus").addClass("taoCirclePlus");
         }
 
         // toggle the .taoSOGpadding and .taoSOGHeaderPadding classes so the
