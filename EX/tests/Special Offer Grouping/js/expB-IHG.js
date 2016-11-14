2cd1accc72bf602ea7a6f92278a1dcd2319b0014
cmdoc <cmdoc@yahoo.com>
2016-11-14 09:21:09 -0500

    Special Offers Group - Fixed "To Rome With Love" bug
    Fixed the bug where the "To Rome With Love" rate package in Rome Italy
    wasn't appearing.  I also added a function that loops through all of the
    new Special Offer Group DIVs and removes any that are empty.

--- EX/tests/Special Offer Grouping/js/expB-IHG.js
+++ EX/tests/Special Offer Grouping/js/expB-IHG.js
@@ -2,8 +2,8 @@ jQuery("document").ready(function(){
 
     // Create image tags for the plus and minus icons for use in the SOGHeader
     // and SOGFooter
-    var taoCirclePlusHref = "https://ihg-my.sharepoint.com/personal/chris_daquin_ihg_com/_layouts/15/guestaccess.aspx?guestaccesstoken=NI3JGUXvFU1Edem4jW40rbx1j3hDG1ORgA2eHlNrMvo%3d&docid=071c63e9874ee4975a2f421c2a80c2070&rev=1";
-    var taoCircleMinusHref = "https://ihg-my.sharepoint.com/personal/chris_daquin_ihg_com/_layouts/15/guestaccess.aspx?guestaccesstoken=556cMcaijVcAzM%2bUgvQ0ubkDwkiltBHb53ZiyrhTZgU%3d&docid=03941d06e633344d49850e91604c85ecb&rev=1";
+    var taoCirclePlusHref = "http://prod.internal.ihg.com/content/dam/etc/media_library/branded/6c/cn/icons/IHG_plus_sign_icon.png";
+    var taoCircleMinusHref = "http://prod.internal.ihg.com/content/dam/etc/media_library/branded/6c/cn/icons/IHG_minus_sign_icon.png";
     var taoCirclePlus = "<img class='taoCirclePlus' href='"+ taoCirclePlusHref + "' />";
     var taoCircleMinus = "<img class='taoCircleMinus' href='"+ taoCircleMinusHref + "' />";
 
@@ -40,6 +40,16 @@ jQuery("document").ready(function(){
 
     });
 
+    // Loop through all of the new .taoSpecialOfferGroup DIVs and remove those
+    // that don't have any rates (in other words, they are empty).
+    jQuery(".taoSpecialOfferGroup").each(function() {
+
+        if (jQuery(this).find(".taoSOGrates .regularRates").length == 0) {
+            jQuery(this).remove();
+        }
+
+    });
+
     // Put in all the code and functionality to cause the new button to open
     // and close the grouped rates.
     jQuery(".taoSOGHeader span, .taoSOGFooter input, img.taoCirclePlus, img.taoCircleMinus").on("click", function () {
@@ -55,7 +65,7 @@ jQuery("document").ready(function(){
         if ($taoSOGHeaderImg.hasClass("taoCirclePlus")) {
             $taoSOGHeaderImg.attr("href", taoCircleMinusHref).removeClass("taoCirclePlus").addClass("taoCircleMinus");
         } else {
-            $taoSOGHeaderImg.attr("href", taoCircleMinusHref).removeClass("taoCircleMinus").addClass("taoCirclePlus");
+            $taoSOGHeaderImg.attr("href", taoCirclePlusHref).removeClass("taoCircleMinus").addClass("taoCirclePlus");
         }
 
         // toggle the .taoSOGpadding and .taoSOGHeaderPadding classes so the
