a136d5ca98905c69bc9a4c5a7f076dfb7fcc6539
cmdoc <cmdoc@yahoo.com>
2016-11-18 08:02:09 -0500

    3 Rate Pack - Best Flex rate row finished
    Finished the coding for the Best Flex rate row. Now starting to work on
    the YOUR RATE Best Flex row.

--- IHG/tests/3 Rate Pack/js/expB.js
+++ IHG/tests/3 Rate Pack/js/expB.js
@@ -5,6 +5,9 @@ jQuery("document").ready(function(){
     var taoDownCaret = "<svg class='taoCaret'><use xlink:href='#tao_down_caret' /></svg>";
     var taoUpCaret = "<svg class='taoCaret'><use xlink:href='#tao_up_caret' /></svg>";
 
+    // Find the Best Flex +1,000 points row (IKME3) and save it to a variable
+    var $taoRateRowTemplate = jQuery("div.regularRates div.rateTypeLineItem input[value='IKME3']").eq(0).closest("div.regularRates");
+
     // Start working on the page, hitting one rate type at a time
     jQuery(".rateTypeLineItems").each(function(i) {
 
@@ -47,17 +50,91 @@ jQuery("document").ready(function(){
                 // their proper place, either in the 3RP div (if it is a
                 // IGCOR or IDME1 rate) or in the SOG div (if it is not IGCOR
                 // or IDME1).
-                jQuery("#tao3RP" + i).prepend(jQuery(this));
+
+                // Create a clone of each upsellContainer. We will use these
+                // quite a lot, so no need to keep searching for them every
+                // time.
+                var $taoUpsellContainer_0 = jQuery(this).find("#upsellContainer_0").clone();
+                var $taoUpsellContainer_1 = jQuery(this).find("#upsellContainer_1").clone();
+                var $taoUpsellContainer_2 = jQuery(this).find("#upsellContainer_2").clone();
+
+debugger;
+
+                /****** IGCOR ******/
+                // Let's tackle the easiest first -- the Best Flex rate.
+                // First, check to see if the Best Flex rate is in the
+                // expected place. If so, then clone the template into a new
+                // Best Flex Rate Row and make changes to that new row.
+                // We need to look for the input named "originalRateCode" and
+                // see if it ends in IGCOR.
+                if ($taoUpsellContainer_0.find("input[name=originalRateCode]").val().indexOf("IGCOR") > -1) {
+
+                    // make a clone of the template for the new Best Flex row
+                    var $taoBestFlexRate = taoCloneRateRowTemplate($taoRateRowTemplate);
+
+                    // Grab the Best Flex rate bullet points and put them in the
+                    // right place
+                    var $taoBFBulletPoints = $taoUpsellContainer_0.find(".rateInfoArea");
+                    $taoBestFlexRate.find(".rateInfoArea").remove();
+                    $taoBestFlexRate.find("div.rateTypeLineItem div.rateTypeLineItemLeft").append($taoBFBulletPoints);
+
+                    // Now grab the rate details link and put it in place of the
+                    // last bullet point, which should replace the "Most Popular
+                    // Rate" line item
+                    var $taoBFRateDetails = $taoUpsellContainer_0.find(".baseRateInfo");
+                    $taoBestFlexRate.find(".defaultRateInfo div:last-child li").html($taoBFRateDetails);
+
+                    // Replace the rate title
+                    var taoRateTitle = "Best Flexible Rate";
+                    var taoRateTitleSpan = "<span class='rateCategory roomOrder'>" + taoRateTitle + "</span>";
+                    $taoBestFlexRate.find("div.rateInfoArea").prepend(taoRateTitleSpan);
+
+                    // Grab the "Book This Room" button and put it into the new row
+                    var $taoBFButton = $taoUpsellContainer_0.find(".rateSelectionArea").eq(0);
+                    $taoBestFlexRate.find(".rateSelectionArea").remove();
+                    $taoBestFlexRate.find(".rateTypeLineItemRight").append($taoBFButton);
+
+                    // Grab the pricing info and put it in
+                    var $taoBFPricing = $taoUpsellContainer_0.find(".mainRateDisplay span.price").eq(0);
+                    $taoBestFlexRate.find("span.price").remove();
+                    $taoBestFlexRate.find("div.priceInfoArea").append($taoBFPricing);
+
+                    // Finally, stick the new Best Flex rate row in the right place
+                    jQuery("#tao3RP" + i).prepend($taoBestFlexRate);
+                }
+
+                /****** IDME1 ******/
+                // Let's tackle the easiest first -- the Best Flex rate.
+                // First, check to see if the Best Flex member rate is in the
+                // expected place. If so, then clone the template into a new
+                // Best Flex Rate Row and make changes to that new row.
+                // We need to look for the input named "memberOiginalRateCode"
+                // and see if it ends in IDME1.
+                if ($taoUpsellContainer_0.find("input[name=memberOiginalRateCode]").val().indexOf("IDME1") > -1) {
+
+                    // make a clone of the template
+                    var $taoMRBestFlexRate = taoCloneRateRowTemplate($taoRateRowTemplate);
+
+                    // Replace the rate title
+                    var taoRateTitle = "YOUR RATE by IHG&reg; Rewards Club";
+                    var taoRateTitleSpan = "<span class='rateCategory roomOrder'>" + taoRateTitle + "</span>";
+                    $taoMRBestFlexRate.find("div.rateInfoArea").prepend(taoRateTitleSpan);
+
+                    // Finally, stick the new Best Flex rate row in the right place
+                    jQuery("#tao3RP" + i).prepend($taoMRBestFlexRate);
+                }
 
             } else {
+
                 // This is one of the many "other" rates. Put this at the end
                 // of the SOG div
                 jQuery("#taoSOG" + i).append(jQuery(this));
+
             }
 
-        });
+        }); // end of looping through .regularRates and .secondaryRates rows
 
-    });
+    }); // end of looping through each room type
 
     // Put in all the code and functionality to cause the new button to open
     // and close the grouped rates.
@@ -87,3 +164,38 @@ jQuery("document").ready(function(){
     jQuery(".viewAllRatesLink").empty();
 
 });
+
+/****** F U N C T I O N S ******/
+function mboxPixelTrack(mbox) {
+    // Code stolen from Adobe's Proactive Chat. This should track click events.
+    var d = new Date();
+    var ub = mboxFactoryDefault.getUrlBuilder().clone();
+    ub.addParameter("mbox", mbox);
+    ub.addParameter('mboxTime', d.getTime() - (d.getTimezoneOffset() * 60000));
+    ub.addParameters(Array.prototype.slice.call(arguments).slice(1));
+    var img = new Image();
+    img.src = ub.buildUrl().replace("/mbox/undefined", "/mbox/ajax");
+    img.style.display = "none";
+    if (document.body) {
+        document.body.insertBefore(img, document.body.firstChild);
+    }
+}
+
+function taoCloneRateRowTemplate ($row) {
+    // clones a template row and sends the clone back for usage
+    var $clone = $row.clone();
+
+    // Remove several things from the new row:
+    //   1. "Bonus Points" image
+    //   2. "Nightly Rate" text
+    //   3. hidden inputs we don't need
+    //   4. more hidden inputs we don't need
+    //   4. hidden span we don't need
+    $clone.find("img.bonusLogo").remove();
+    $clone.find("div.avgratediv").remove();
+    $clone.find("div.rateTypeLineItem > input[type='hidden']").remove();
+    $clone.find("div.rateInfoArea > input[type='hidden']").remove();
+    $clone.find("div.rateTypeLineItem > span").eq(0).remove();
+
+    return $clone;
+}
